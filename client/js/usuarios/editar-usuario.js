4646836bf5379fd0c68f5efe6631e9a76d71011a
Jhonatan Froeder <jhonatan.froeder@aluno.ufms.br>
2019-03-12 11:00:04 -0400

    :pencil2: Alterando página de editar usuário

---
+++ client/js/usuarios/editar-usuario.js
@@ -0,0 +1,3 @@
+function voltar() {
+    window.location.replace('listar-usuarios.html')
+}
--- client/js/usuarios/listar-usuarios.js
+++ client/js/usuarios/listar-usuarios.js
@@ -5,3 +5,27 @@ function voltar() {
 function adicionarUsuario() {
     window.location.replace('cadastrar-usuario.html')
 }
+
+function editarUsuario() {
+    var table = document.getElementById('usuarios');
+    let id = ''
+
+    for (var i = 1; i < table.rows.length; i++) {
+        let teste = id
+
+        table.rows[i].onclick = function () {
+            //rIndex = this.rowIndex;
+            teste = this.cells[0].innerHTML
+            id = teste
+            let aux = id
+
+            window.location.replace('editar-usuario.html?id=' + aux + '')
+        };
+
+    }
+
+}
+
+function excluirUsuario() {
+
+}
